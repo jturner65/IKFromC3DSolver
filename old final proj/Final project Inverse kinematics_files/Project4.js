0c7c6f0c27bb426301a2b541ee6feb02fca7b607
john turner <jturner65@gatech.edu>
2016-08-03 11:07:00 -0400

    --init commit

---
+++ old final proj/Final project Inverse kinematics_files/Project4.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20120816
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Project4_files/Project4Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
